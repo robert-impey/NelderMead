0e1d4733b654ad52908e3d495b0fffb825910998
Robert Impey <robert@impey.info>
2012-06-10 17:59:17 +0100

    Added simple options.

--- NelderMead/Main.cpp
+++ NelderMead/Main.cpp
@@ -1,25 +1,37 @@
 #include <iostream>
+#include <string>
+
 using namespace std;
 
 double f(double, double);
 
-int main()
+int main(int argc, char* argv[])
 {
-	cout << "Nelder-Mead Method\n";
+	if (argc == 1) {
+		cout << "Nelder-Mead Method\n";
+	} else {
+		string tool = argv[1];
 
-	const double xMin = 0;
-	const double xMax = 10;
-	const double yMin = 0;
-	const double yMax = 10;
+		if (tool == "Print") {
+			string tool = argv[1];
+
+			const double xMin = 0;
+			const double xMax = 10;
+			const double yMin = 0;
+			const double yMax = 10;
 	
-	const double xStep = 0.1;
-	const double yStep = 0.1;
+			const double xStep = 0.1;
+			const double yStep = 0.1;
 
-	for (double x = xMin; x <= xMax; x += xStep) {
-		for (double y = yMin; y <= yMax; y += yStep) {
-			cout << f(x, y) << ",";
+			for (double x = xMin; x <= xMax; x += xStep) {
+				for (double y = yMin; y <= yMax; y += yStep) {
+					cout << f(x, y) << ",";
+				}
+				cout << "\n";
+			}
+		} else {
+			cerr << "Unknown tool!\n";
 		}
-		cout << "\n";
 	}
 
 	return 0;
